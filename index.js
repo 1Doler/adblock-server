7142ae422cf9d43884ff47a46f6526663a3cde90
daler <kabilovd7@gmail.com>
2023-11-17 18:04:38 +0300

    new index

--- index.js
+++ index.js
@@ -57,6 +57,21 @@ app.get('/html', (req, res) => {
         fileStream.pipe(res);
     });
 });
+app.get('/html2', (req, res) => {
+    // Путь к файлу, который вы хотите вернуть
+    const filePath = path.join(__dirname, 'index2.html');
+
+    // Проверка существования файла
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            return res.status(404).send('File not found');
+        }
+
+        // Чтение файла и отправка его содержимого в ответе
+        const fileStream = fs.createReadStream(filePath);
+        fileStream.pipe(res);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
