dea48dc3dbbbb50c2b348f2ce0689c32dc6b9d5c
Daler Kabilov <d.kabilov@adriver.ru>
2023-11-22 17:09:51 +0300

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const publicPath = path.join(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
+app.get('/img', (req, res) => {
     // Путь к файлу, который вы хотите вернуть
     const filePath = path.join(__dirname, 'reklama.jpg');
 
